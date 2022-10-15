1f0db24bbe403de84fa035c5eac8d8f925317e0c
Seun Ajetomobi <fullstackfory@gmail.com>
2022-10-15 15:06:53 +0000

    Complete product edit and add functionality

--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
-from django.utils.html import format_html
 
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
-    def image_tag(self, obj):
-        return format_html('<img src="{}"  width="100" height="100"/>'.format(obj.image.url))
-
-    image_tag.short_description = 'Image'
     list_display = (
         'sku',
         'name',
@@ -17,7 +12,6 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
-        'image_tag',
     )
 
     ordering = ('sku',)
