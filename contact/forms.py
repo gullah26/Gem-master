1177f221b376858b22a2cf1c0fccb4b22a5eb37b
Seun Ajetomobi <fullstackfory@gmail.com>
2022-10-21 03:55:40 +0000

    code formating

--- contact/forms.py
+++ contact/forms.py
@@ -8,4 +8,5 @@ class ContactForm(forms.Form):
     full_name = forms.CharField(max_length=55, required=True)
     email = forms.EmailField(max_length=254, required=True)
     subject = forms.CharField(max_length=55, required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True, max_length=1000)
+    message = forms.CharField(widget=forms.Textarea,
+                              required=True, max_length=1000)
